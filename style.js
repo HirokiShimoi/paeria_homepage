4cbaa9964877f4091e02b1c274a0d56d8e6c3caf
Hiroki Shimoie <apelox1gmail.com>
2023-08-25 00:43:18 +0900

    content充実させた

--- style.js
+++ style.js
@@ -19,3 +19,30 @@ document.addEventListener('DOMContentLoaded', function() {
   logoContainer.style.animationDuration = (logoWidth / 100) + 's';
 
 });
+
+
+var swiper = new Swiper('.swiper-container', {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 30,
+  speed: 300,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
